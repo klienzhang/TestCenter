f8c9387480f4b231c1639e91509896456260d592
klienzhang <klienly@aliyun.com>
2016-09-22 18:57:47 +0800

    Bug修复

--- TestCenterServer/src/main/java/com/berserker/server/controller/tools/GeneratorController.java
+++ TestCenterServer/src/main/java/com/berserker/server/controller/tools/GeneratorController.java
@@ -3,26 +3,32 @@ package com.berserker.server.controller.tools;
 import com.berserker.server.service.ToolsBasicService;
 import com.berserker.testcenterapi.model.ClientResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Controller
 @RequestMapping(value = "/tools")
 public class GeneratorController {
 
     @Autowired
     private ToolsBasicService service;
 
+
     @RequestMapping(value = "/generator")
     public String getGeneratorPage(){
         return "generator";
     }
 
+    @ResponseBody
     @RequestMapping(value = "/generator/getBankList")
     public ClientResponse getBankList(){
         return service.getBankList();
     }
 
+    @ResponseBody
     @RequestMapping(value = "/generator/getIdCard")
     public ClientResponse getIdCard(HttpServletRequest request){
         return service.getBankCardNum(request);
